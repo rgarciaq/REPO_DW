ab067e8743f14d9772169561be6ea936dfea403f
rgarciaq <rgarciaq03@educarex.es>
2021-09-20 10:18:56 +0200

    Ejercicio 3 html servlet

---
+++ Prueba0/src/main/java/ej3.java
@@ -0,0 +1,76 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class ej3
+ */
+@WebServlet("/ej3")
+public class ej3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ej3() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre=request.getParameter("nombre");
+		String fav_language=request.getParameter("fav_language");
+		String ide1=request.getParameter("ide1");
+		String ide2=request.getParameter("ide2");
+		String ide3=request.getParameter("ide3");
+		String [] arrayEstudio=request.getParameterValues("estudio");
+		
+		response.setContentType("text/html");
+		PrintWriter salida = response.getWriter();
+		
+		salida.println("Datos del Servlet");
+		salida.println("<br>");
+		salida.println("Nombre: "+nombre);
+		salida.println("<br>");
+		salida.println("Lenguaje Favorito: "+fav_language);
+		salida.println("<br>");
+		String ides="";
+		if(null!=ide1) {
+			ides=ide1;
+		}
+		if(null!=ide2) {
+			ides += " "+ide2;
+		}
+		if(null!=ide3) {
+			ides += " "+ide3;
+		}
+		salida.println("Lenguaje: "+ides);
+		
+		String estudio="";
+		for(int i=0;i<arrayEstudio.length;i++) {
+			estudio+= arrayEstudio[i]+" ";
+		}
+		salida.println("<br>");
+		salida.println("Lugar de trabajo: " +estudio);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
